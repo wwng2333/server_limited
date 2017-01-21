bf5e23b859b4a6b369b9de82c9150cbcdd546f33
wwng2333 <wwng@2333.me>
2017-01-22 05:22:58 +0800

    Delete wechat.php

--- wechat.php
+++
@@ -1,8 +0,0 @@
-<?php
-$api = 'http://localhost/online.php';
-$array = json_decode(file_get_contents($api), true);
-$count = count($array) - 1;
-if(strstr($array[$count],'form method') {
-	$url = 'http://sc.ftqq.com/YOUR_API_KEY.send?text='.urlencode($array[0].'ÉÏ»õÁË');
-	file_get_contents($url);
-}
